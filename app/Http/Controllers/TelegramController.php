a1b4e4e69f25d82eb04e9a9e4a52bb72f8d6cc27
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-11-23 17:59:15 +0800

    Update TelegramController.php
    去除自动发种

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -45,7 +45,7 @@ class TelegramController extends Controller
                 if (isset($update['message']['new_chat_members'])) {
                     $newMembers = $update['message']['new_chat_members'];
                     foreach ($newMembers as $member) {
-                        $welcomeMessage = "欢迎 " . $member['first_name'] . " 来到Kimoji，申请内测账号请上翻聊天记录或回复阿K";
+                        $welcomeMessage = "欢迎 " . $member['first_name'] . " 来到Kimoji，自动获取内测账号渠道已关闭，请在群内留言取药";
                         Telegram::sendMessage([
                             'chat_id' => $chatId,
                             'text' => $welcomeMessage
@@ -54,29 +54,29 @@ class TelegramController extends Controller
                     return response()->json(['status' => 'success']);
                 }
                 //自动发药，申请内测资格
-                if (preg_match('/申请内测资格\s*([\w\.\-]+@\w+\.\w+)/', $text, $matches)) {
-                    $email = $matches[1];
-
-                    if ($this->isValidEmail($email)) {
-                        try {
-                            $this->inviteService->sendInvite($email);
-                            Telegram::sendMessage([
-                                'chat_id' => $chatId,
-                                'text' => "内测邀请函已发送至: {$email}，注册后请勿再使用该指令，也不要向任何群外人员发送邀请，否则将取消内测资格，感谢配合"
-                            ]);
-                        } catch (\Exception $e) {
-                            Telegram::sendMessage([
-                                'chat_id' => $chatId,
-                                'text' => "发送失败，失败原因：" . $e->getMessage()
-                            ]);
-                        }
-                    } else {
-                        Telegram::sendMessage([
-                            'chat_id' => $chatId,
-                            'text' => "提供的邮箱地址不正确，发送失败"
-                        ]);
-                    }
-                }
+//                if (preg_match('/申请内测资格\s*([\w\.\-]+@\w+\.\w+)/', $text, $matches)) {
+//                    $email = $matches[1];
+//
+//                    if ($this->isValidEmail($email)) {
+//                        try {
+//                            $this->inviteService->sendInvite($email);
+//                            Telegram::sendMessage([
+//                                'chat_id' => $chatId,
+//                                'text' => "内测邀请函已发送至: {$email}，注册后请勿再使用该指令，也不要向任何群外人员发送邀请，否则将取消内测资格，感谢配合"
+//                            ]);
+//                        } catch (\Exception $e) {
+//                            Telegram::sendMessage([
+//                                'chat_id' => $chatId,
+//                                'text' => "发送失败，失败原因：" . $e->getMessage()
+//                            ]);
+//                        }
+//                    } else {
+//                        Telegram::sendMessage([
+//                            'chat_id' => $chatId,
+//                            'text' => "提供的邮箱地址不正确，发送失败"
+//                        ]);
+//                    }
+//                }
             } else {
                 // 如果没有 'text' 字段，记录一个不同的消息
                 Log::info("Received a non-text message type");
@@ -90,7 +90,7 @@ class TelegramController extends Controller
     {
         $helpMessage = "阿K当前支持的指令:\n";
         $helpMessage .= "阿K - 显示帮助信息\n";
-        $helpMessage .= "申请内测资格 [邮箱] - 申请加入内测";
+        #$helpMessage .= "申请内测资格 [邮箱] - 申请加入内测";
 
         Telegram::sendMessage([
             'chat_id' => $chatId,
