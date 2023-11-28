df2295c5c8aa088321025423f82968538e66e318
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-11-28 20:30:41 +0800

    Update test_email.blade.php

--- resources/views/emails/test_email.blade.php
+++ resources/views/emails/test_email.blade.php
@@ -2,5 +2,5 @@
 # Test Email
 Your test email has been successfully delivered! Looks like your mail configs are on point!
 Thanks,
-{{ config('other.title') }}
+{{ __('email.register-footer') }}
 @endcomponent
