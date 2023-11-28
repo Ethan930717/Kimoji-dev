a91686400b1c7f90b5f5411f41d9f96efe657482
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-11-28 20:27:11 +0800

    Update test_email.blade.php

--- resources/views/emails/test_email.blade.php
+++ resources/views/emails/test_email.blade.php
@@ -1,18 +1,9 @@
 @component('mail::message')
-    # {{ __('email.invite-header') }} {{ config('other.title') }} !
-
-    <div style="background-color:#f8f9fa; padding:10px; border-radius:5px;">
-        <p style="color:#333; font-size:16px;">
-            <strong>{{ __('email.invite-message') }}:</strong> {{ __('email.invite-invited') }}
-        </p>
-    </div>
-
-    <!-- 示例图片 -->
-    <img src="https://kimoji.club/img/indexlogo.png" alt="logo" style="width:100%; max-width:600px; height:auto; margin-top:20px;">
-    
-
-    <p>{{ __('email.register-footer') }}</p>
-    <p style="word-wrap: break-word; overflow-wrap: break-word; word-break: break-word;">
-        {{ route('register', $invite->code) }}
-    </p>
+# Test Email
+Your test email has been successfully delivered! Looks like your mail configs are on point!
+Thanks,
+{{ config('other.title') }}
+@endcomponent
+@endcomponent
+<p>{{ __('email.register-footer') }}</p>
 @endcomponent
