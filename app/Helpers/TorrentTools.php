fe211ffbc1a4ecf0c34d9135579f2d5b03e13f77
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-10 20:54:17 +0800

    1

--- app/Helpers/TorrentTools.php
+++ app/Helpers/TorrentTools.php
@@ -201,9 +201,9 @@ class TorrentTools
         return !(
             \strlen($filename) > 255
             // nodes containing: `\`, `/`, `?`, `<`, `>`, `:`, `8`, `|`, and ascii characters from 0 through 31
-            || preg_match('/[\\\\\\/?<>:*|"\x00-\x1f]/', $filename)
+            // || preg_match('/[\\\\\\/?<>:*|"\x00-\x1f]/', $filename)
             // nodes only containing one or many: `.`; or only containing one or many `.`, ` `.
-            || preg_match('/(^\\.+|[. ]+)$/', $filename)
+            // || preg_match('/(^\\.+|[. ]+)$/', $filename)
             // Special windows filenames.
             || preg_match('/^(con|prn|aux|nul|com\d|lpt\d)(\\..*)?$/i', $filename)
             // BitComet padding files
