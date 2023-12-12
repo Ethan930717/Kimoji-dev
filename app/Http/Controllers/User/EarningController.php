e16b867d462530b6ccb66b2a895678290ff6f8fd
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-12 22:16:30 +0800

    Update EarningController.php

--- app/Http/Controllers/User/EarningController.php
+++ app/Http/Controllers/User/EarningController.php
@@ -58,7 +58,7 @@ class EarningController extends Controller
      */
     private function calculateBonusPerHour($bytes) {
         $gb = $bytes / (1024 * 1024 * 1024); // 将字节转换为 GB
-        $bonusPerGb = 0.09765625; // 每 GB 的魔力增加量
+        $bonusPerGb = 0.1; // 每 GB 的魔力增加量
 
         return $gb * $bonusPerGb; // 总保种 GB 数乘以每 GB 的魔力增加量
     }
