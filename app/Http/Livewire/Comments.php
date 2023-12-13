38f1db58426c9cd0b9c4c94d719cfcb432fd78a7
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-13 19:18:15 +0800

    1

--- app/Http/Livewire/Comments.php
+++ app/Http/Livewire/Comments.php
@@ -85,11 +85,11 @@ class Comments extends Component
 
     final public function postComment(): void
     {
-        if ($this->user->can_comment === false) {
+/*        if ($this->user->can_comment === false) {
             $this->dispatchBrowserEvent('error', ['type' => 'error',  'message' => trans('comment.rights-revoked')]);
 
             return;
-        }
+        }*/
 
  /*       if (strtolower(class_basename($this->model)) === 'torrent' && $this->model->status !== Torrent::APPROVED) {
             $this->dispatchBrowserEvent('error', ['type' => 'error',  'message' => trans('comment.torrent-status')]);
