18d791ff34fc006f62d35b90411b43589e2103cd
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-15 19:33:32 +0800

    1

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,56 +1,54 @@
 @component('mail::message')
-    {{-- Greeting --}}
-    @if (! empty($greeting))
-        # {{ $greeting }}
-    @else
-        @if ($level === 'error')
-            # {{ __('哎呀') }}
-        @else
-            # {{ __('您好') }}
-        @endif
-    @endif
+{{-- Greeting --}}
+@if (! empty($greeting))
+# {{ $greeting }}
+@else
+@if ($level === 'error')
+# {{ __('哎呀') }}
+@else
+# {{ __('您好') }}
+@endif
+@endif
 
-    {{-- Intro Lines --}}
-    欢迎您加入KIMOJI，请点击下面的按钮来验证您的电子邮件地址。
+{{-- Intro Lines --}}
+欢迎您加入KIMOJI，请点击下面的按钮来验证您的电子邮件地址。
 
-    {{-- Action Button --}}
-    @isset($actionText)
-        <?php
-        switch ($level) {
-            case 'success':
-            case 'error':
-                $color = $level;
-                break;
-            default:
-                $color = 'primary';
-        }
-        ?>
-        @component('mail::button', ['url' => $actionUrl, 'color' => $color])
-            {{ $actionText }}
-        @endcomponent
-    @endisset
+{{-- Action Button --}}
+@isset($actionText)
+<?php
+switch ($level) {
+case 'success':
+case 'error':
+$color = $level;
+break;
+default:
+$color = 'primary';
+}
+?>
+@component('mail::button', ['url' => $actionUrl, 'color' => $color])
+{{ $actionText }}
+@endcomponent
+@endisset
 
-    {{-- Outro Lines --}}
-    @foreach ($outroLines as $line)
-        {{ $line }}
+{{-- Outro Lines --}}
+如果您不知晓本条邮件所述内容，请您无需理会
 
-    @endforeach
-    {{-- Salutation --}}
-    @if (! empty($salutation))
-        {{ $salutation }}
-    @else
-    KIMOJI 敬上
-    @endif
+{{-- Salutation --}}
+@if (! empty($salutation))
+{{ $salutation }}
+@else
+KIMOJI 敬上
+@endif
 
-    {{-- Subcopy --}}
-    @isset($actionText)
-        @slot('subcopy')
-            {{ __(
-                "如果您无法点击按钮, 请尝试访问以下链接:\n\n",
-                [
-                    'actionText' => $actionText,
-                ]
-            ) }} <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-        @endslot
-    @endisset
+{{-- Subcopy --}}
+@isset($actionText)
+@slot('subcopy')
+{{ __(
+"如果您无法点击 \":actionText\" 按钮, 请手动访问链接:\n\n",
+[
+'actionText' => $actionText,
+]
+) }} <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+@endslot
+@endisset
 @endcomponent
