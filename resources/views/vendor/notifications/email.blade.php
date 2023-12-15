3f99fd23734d02eb3d310d8f5f1084a9f804eeff
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-15 16:07:03 +0800

    Update email.blade.php

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -1,61 +1,61 @@
 @component('mail::message')
-{{-- Greeting --}}
-@if (! empty($greeting))
-# {{ $greeting }}
-@else
-@if ($level === 'error')
-# {{ __('哎呀') }}
-@else
-# {{ __('喔唷') }}
-@endif
-@endif
+    {{-- Greeting --}}
+    @if (! empty($greeting))
+        # {{ $greeting }}
+    @else
+        @if ($level === 'error')
+            # {{ __('哎呀') }}
+        @else
+            # {{ __('喔唷') }}
+        @endif
+    @endif
 
-{{-- Intro Lines --}}
-@foreach ($introLines as $line)
-{{ $line }}
+    {{-- Intro Lines --}}
+    @foreach ($introLines as $line)
+        {{ $line }}
 
-@endforeach
+    @endforeach
 
-{{-- Action Button --}}
-@isset($actionText)
-<?php
-    switch ($level) {
-        case 'success':
-        case 'error':
-            $color = $level;
-            break;
-        default:
-            $color = 'primary';
-    }
-?>
-@component('mail::button', ['url' => $actionUrl, 'color' => $color])
-{{ $actionText }}
-@endcomponent
-@endisset
+    {{-- Action Button --}}
+    @isset($actionText)
+            <?php
+            switch ($level) {
+                case 'success':
+                case 'error':
+                    $color = $level;
+                    break;
+                default:
+                    $color = 'primary';
+            }
+            ?>
+        @component('mail::button', ['url' => $actionUrl, 'color' => $color])
+            {{ $actionText }}
+        @endcomponent
+    @endisset
 
-{{-- Outro Lines --}}
-@foreach ($outroLines as $line)
-{{ $line }}
+    {{-- Outro Lines --}}
+    @foreach ($outroLines as $line)
+        {{ $line }}
 
-@endforeach
+    @endforeach
 
-{{-- Salutation --}}
-@if (! empty($salutation))
-{{ $salutation }}
-@else
-{{ config('app.name') }}
-@endif
+    {{-- Salutation --}}
+    @if (! empty($salutation))
+        {{ $salutation }}
+    @else
+        {{ __('Regards') }},<br>
+        {{ config('app.name') }}
+    @endif
 
-{{-- Subcopy --}}
-@isset($actionText)
-@slot('subcopy')
-{{ __(
-    "如果您无法点击 \":actionText\" 按钮, 请手动访问以下链接:',
-    [
-        'actionText' => $actionText,
-    ]
-) }}
-<span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
-@endslot
-@endisset
+    {{-- Subcopy --}}
+    @isset($actionText)
+        @slot('subcopy')
+            {{ __(
+                "如果您无法点击 \":actionText\" 按钮, 请手动访问链接:\n",
+                [
+                    'actionText' => $actionText,
+                ]
+            ) }} <span class="break-all">[{{ $displayableActionUrl }}]({{ $actionUrl }})</span>
+        @endslot
+    @endisset
 @endcomponent
