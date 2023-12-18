a16b93c7c4fe1a49f691382a1c8a36ed98d29837
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-19 01:17:12 +0800

    1

--- resources/js/unit3d/optional.js
+++ resources/js/unit3d/optional.js
@@ -1,29 +1,22 @@
 function imageGallery(images) {
     return {
-        images: images && images.length > 0 ? images.map(v => v.url) : [],
+        images: images && images.length > 0? images.map(v=>v.url) : [], // Populate this array with your image URLs
         currentSlide: 0,
         showModal: false,
-        imageTransition: false,
 
         openModal(index) {
             this.currentSlide = index;
             this.showModal = true;
-            this.imageTransition = true;
         },
 
         closeModal() {
             this.showModal = false;
-            this.imageTransition = false;
         },
 
         changeSlide(n) {
-            this.imageTransition = false;
-            this.$nextTick(() => {
-                this.currentSlide += n;
-                if (this.currentSlide >= this.images.length) this.currentSlide = 0;
-                if (this.currentSlide < 0) this.currentSlide = this.images.length - 1;
-                this.imageTransition = true;
-            });
+            this.currentSlide += n;
+            if (this.currentSlide >= this.images.length) this.currentSlide = 0;
+            if (this.currentSlide < 0) this.currentSlide = this.images.length - 1;
         }
     }
 }
