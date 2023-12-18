c4968d03a4fb4438573a481bee24d074d677ea76
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-18 17:51:53 +0800

    1

--- resources/js/app.js
+++ resources/js/app.js
@@ -47,18 +47,63 @@ if (document.getElementById('vue')) {
 window.Swal = require('sweetalert2');
 
 // howler js
-import { Howl } from 'howler';
+import { Howl, Howler } from 'howler';
 
 document.addEventListener('DOMContentLoaded', function () {
-    var playButton = document.getElementById('play-sound');
-    var soundPath = playButton.getAttribute('data-sound-path');
+    var audioPlayer = document.getElementById('audio-player');
+    var soundPath = audioPlayer.getAttribute('data-sound-path');
+    var playButton = document.getElementById('play-btn');
+    var pauseButton = document.getElementById('pause-btn');
+    var volumeControl = document.getElementById('volume-control');
 
     var sound = new Howl({
-        src: [soundPath]
+        src: [soundPath],
+        html5: true,
+        volume: volumeControl.value
     });
 
     playButton.addEventListener('click', function() {
         sound.play();
     });
+
+    pauseButton.addEventListener('click', function() {
+        sound.pause();
+    });
+
+    volumeControl.addEventListener('input', function() {
+        sound.volume(this.value);
+    });
+
+
+    // 频率显示
+    var canvas = document.getElementById('frequency-display');
+    var ctx = canvas.getContext('2d');
+
+    // 使用 Howler 的 FFT 功能显示频率（需要 Web Audio API）
+    if (Howler.ctx) {
+        var analyser = Howler.ctx.createAnalyser();
+        Howler.masterGain.connect(analyser);
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+
+        function draw() {
+            requestAnimationFrame(draw);
+            analyser.getByteFrequencyData(dataArray);
+            ctx.fillStyle = 'rgb(200, 200, 200)';
+            ctx.fillRect(0, 0, canvas.width, canvas.height);
+            var barWidth = (canvas.width / bufferLength) * 2.5;
+            var barHeight;
+            var x = 0;
+
+            for(var i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i];
+                ctx.fillStyle = 'rgb(' + (barHeight+100) + ',50,50)';
+                ctx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+                x += barWidth + 1;
+            }
+        }
+        draw();
+    }
 });
 
