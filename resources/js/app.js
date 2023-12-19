f5a715f2152c0819fb532dab99b62c686266d220
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-20 00:48:16 +0800

    Aplayer

--- resources/js/app.js
+++ resources/js/app.js
@@ -46,132 +46,18 @@ if (document.getElementById('vue')) {
 // Sweet Alert
 window.Swal = require('sweetalert2');
 
+import APlayer from 'aplayer';
+document.addEventListener('DOMContentLoaded', function() {
+    // 获取封面图片 URL
+    const coverUrl = document.getElementById('aplayer-container').dataset.cover;
 
-
-//Howler
-import { Howl, Howler } from 'howler';
-const audioContainer = document.getElementById('audio-container');
-const soundSrc = audioContainer.getAttribute('data-sound-src');
-
-const sound = new Howl({
-    src: [soundSrc],
-    autoplay: false,
-    html5: true, // 开启流式播放
-    loop: false,
-    volume: 1.0,
-    onend: function() {
-        requestAnimationFrame(updateProgress);
-        console.log('Finished playing');
-    }
+    const ap = new APlayer({
+        container: document.getElementById('aplayer'),
+        audio: [{
+            name: '倒带',
+            artist: '蔡依琳', // 可以替换成实际的艺术家名
+            url: '/sounds/daodai.mp3',
+            cover: coverUrl // 使用从 Blade 模板获取的封面图片 URL
+        }]
+    });
 });
-//音量控制
-const volumeSlider = document.getElementById('volume-slider');
-volumeSlider.addEventListener('input', function() {
-    sound.volume(this.value);
-});
-
-//播放/暂停
-const togglePlayPauseButton = document.getElementById('play-pause-button');
-togglePlayPauseButton.addEventListener('click', function() {
-    if (sound.playing()) {
-        sound.pause();
-    } else {
-        sound.play();
-    }
-});
-// 更新播放/暂停按钮的图标
-function updatePlayPauseButton() {
-    if (sound.playing()) {
-        togglePlayPauseButton.classList.remove('fa-play');
-        togglePlayPauseButton.classList.add('fa-pause');
-    } else {
-        togglePlayPauseButton.classList.remove('fa-pause');
-        togglePlayPauseButton.classList.add('fa-play');
-    }
-}
-// 静音按钮状态更新
-const muteButton = document.getElementById('mute');
-muteButton.addEventListener('click', function() {
-    sound.mute(!sound.mute());
-    updateMuteButton();
-});
-
-function updateMuteButton() {
-    if (sound.mute()) {
-        muteButton.style.color = '#A3BAEAFF';
-    } else {
-        muteButton.style.color = 'white';
-    }
-}
-
-// 更新播放状态相关的UI
-sound.on('play', updatePlayPauseButton);
-sound.on('pause', updatePlayPauseButton);
-sound.on('end', updatePlayPauseButton);
-
-
-// 获取进度条元素
-const progressBar = document.getElementById('progress-bar');
-function updateProgress() {
-    const seek = sound.seek() || 0;
-    const percent = ((seek / sound.duration()) * 100) || 0;
-    progressBar.style.width = percent + "%";
-    // 更新时间显示
-    document.getElementById('current-time').textContent = formatTime(seek);
-    document.getElementById('total-time').textContent = formatTime(sound.duration());
-    if (sound.playing()) {
-        requestAnimationFrame(updateProgress);
-    }
-}
-// 格式化时间为分:秒
-function formatTime(seconds) {
-    const minutes = Math.floor(seconds / 60);
-    const secondsLeft = Math.floor(seconds % 60);
-    return `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
-}
-updateProgress(); // 初始调用以设置时间
-
-
-//canvas
-// 获取 Howler 的 Web Audio API AudioContext 和 Master GainNode
-const audioCtx = Howler.ctx;
-const masterGain = Howler.masterGain;
-
-// 创建 AnalyserNode
-const analyser = audioCtx.createAnalyser();
-analyser.fftSize = 2048; // FFT 大小，影响细节
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
-
-// 将 AnalyserNode 连接到 Master GainNode
-masterGain.connect(analyser);
-
-// 设置 Canvas
-const canvas = document.getElementById('audioCanvas');
-const canvasCtx = canvas.getContext('2d');
-
-function draw() {
-    // 设置动画循环
-    requestAnimationFrame(draw);
-
-    // 获取频率数据
-    analyser.getByteFrequencyData(dataArray);
-
-    // 使用 Canvas 绘制频率数据
-    canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-
-    const barWidth = (canvas.width / bufferLength) * 2.5;
-    let barHeight;
-    let x = 0;
-
-    for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        canvasCtx.fillStyle = `rgb(${barHeight + 100},50,50)`;
-        canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
-
-        x += barWidth + 1;
-    }
-}
-
-draw();
--- resources/views/torrent/partials/music_meta.blade.php
+++ resources/views/torrent/partials/music_meta.blade.php
@@ -2,42 +2,14 @@
     @if (file_exists(public_path().'/files/img/torrent-banner_'.$torrent->id.'.jpg'))
         <img class="meta__backdrop" src="{{ url('/files/img/torrent-banner_'.$torrent->id.'.jpg') }}" alt="Backdrop">
     @endif
-        <a class="meta__title-link">
-            <h1 class="meta__title">
-                {{ $torrent?->name}}
-            </h1>
-        </a>
         <a class="meta__poster-link">
             <img
                     src="{{ file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg') ? url('/files/img/torrent-cover_'.$torrent->id.'.jpg') : 'https://via.placeholder.com/500x500' }}"
                     class="meta__poster"
             >
         </a>
-        <div class="meta__chips">
-            <section class="meta__chip-container">
-                @if (isset($torrent->keywords) && $torrent->keywords->isNotEmpty())
-                    <article class="meta__keywords">
-                        <a class="meta-chip" href="{{ route('torrents.index', ['view' => 'group', 'keywords' => $torrent->keywords->pluck('name')->join(', ')]) }}">
-                            <i class="{{ config('other.font-awesome') }} fa-tag meta-chip__icon"></i>
-                            <h2 class="meta-chip__name">关键词</h2>
-                            <h3 class="meta-chip__value">{{ $torrent->keywords->pluck('name')->join(', ') }}</h3>
-                        </a>
-                    </article>
-                @endif
-            </section>
-        </div>
-        <div id="audio-container" data-sound-src="/sounds/daodai.mp3">
-            <input type="range" id="volume-slider" min="0" max="1" step="0.01" value="1">
-            <button id="play-pause-button" class="fa fas fa-play"></button> <!-- 合并的播放/暂停按钮 -->
-            <button id="mute" class="fa fas fa-volume-mute"></button> <!-- 静音按钮 -->
-            <!-- 进度条和时间显示 -->
-            <div id="progress-container">
-                <div id="progress-bar"></div>
-            </div>
-            <div id="time-display">
-                <span id="current-time">0:00</span> / <span id="total-time">0:00</span>
-            </div>
-            <canvas id="audioCanvas"></canvas>
+        <div id="aplayer-container" data-cover="{{ file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg') ? url('/files/img/torrent-cover_'.$torrent->id.'.jpg') : 'https://via.placeholder.com/400x600' }}">
+            <div id="aplayer" class="aplayer"></div>
         </div>
         <div class="meta__actions">
         <a class="meta__dropdown-button" href="#">
@@ -57,5 +29,17 @@
         </ul>
     </div>
 
-
+    <div class="meta__chips">
+        <section class="meta__chip-container">
+            @if (isset($torrent->keywords) && $torrent->keywords->isNotEmpty())
+                <article class="meta__keywords">
+                    <a class="meta-chip" href="{{ route('torrents.index', ['view' => 'group', 'keywords' => $torrent->keywords->pluck('name')->join(', ')]) }}">
+                        <i class="{{ config('other.font-awesome') }} fa-tag meta-chip__icon"></i>
+                        <h2 class="meta-chip__name">关键词</h2>
+                        <h3 class="meta-chip__value">{{ $torrent->keywords->pluck('name')->join(', ') }}</h3>
+                    </a>
+                </article>
+            @endif
+        </section>
+    </div>
 </section>
