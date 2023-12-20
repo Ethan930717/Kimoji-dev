88461a6716e171ea36dac858cdeea5140ad8c724
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-20 19:00:18 +0800

    1

--- resources/views/torrent/partials/music_meta.blade.php
+++ resources/views/torrent/partials/music_meta.blade.php
@@ -14,9 +14,21 @@
             >
         </a>
         <div class="meta__actions">
-            <div id="aplayer-container" data-cover="{{ url('img/kimoji-music.webp') }}">
-                <div id="aplayer" class="aplayer"></div>
-            </div>
+            <a class="meta__dropdown-button" href="#">
+                <i class="{{ config('other.font-awesome') }} fa-ellipsis-v"></i>
+            </a>
+            <ul class="meta__dropdown">
+                <li>
+                    <a href="{{ route('torrents.create', ['category_id' => $category->id, 'title' => rawurlencode($meta->title ?? '') ?? 'Unknown', 'imdb' => $torrent?->imdb ?? '', 'tmdb' => $meta?->id ?? '']) }}">
+                        {{ __('common.upload') }}
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('requests.create', ['title' => rawurlencode($meta?->title ?? '') ?? 'Unknown', 'imdb' => $torrent?->imdb ?? '', 'tmdb' => $meta?->id ?? '']) }}">
+                        申请补档
+                    </a>
+                </li>
+            </ul>
         </div>
         <ul class="meta__ids">
             <li class="meta__imdb">
@@ -42,5 +54,7 @@
             @endif
         </ul>
         <p class="meta__description">王朝是一个新的具有里程碑意义的系列。这个系列将记录世界上最具标志性的动物的故事，在世界最知名的地点，他们为了维系自己的王朝而努力奋斗着。在每一集里面，他们每个个体的故事都会通过激烈的、引人入胜的戏剧情节展开。</p>
-
-</section>
+        <div id="aplayer-container" data-cover="{{ url('img/kimoji-music.webp') }}">
+            <div class="aplayer-title">单曲试听</div>
+            <div id="aplayer" class="aplayer"></div>
+        </div></section>
