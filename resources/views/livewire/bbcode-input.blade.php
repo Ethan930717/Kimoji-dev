1dbf22f65869dfaf419ce82d88b7713395664815
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-24 16:16:54 +0800

    Update bbcode-input.blade.php

--- resources/views/livewire/bbcode-input.blade.php
+++ resources/views/livewire/bbcode-input.blade.php
@@ -322,6 +322,7 @@
 
         },
         showButtons: false,
+        showOneClickButtons: false,
         bbcodePreviewHeight: null,
         isPreviewEnabled: @entangle('isPreviewEnabled'),
         isOverInput: false,
@@ -511,8 +512,8 @@
         <li><button type="button" class="form__button form__button--text" x-on:click="confirm-making-announce">制作说明</button></li>
         <li><button type="button" class="form__button form__button--text" x-on:click="insertWithCheck('[center][color=#bbff88][size=24][b][spoiler=截图赏析]', '[/spoiler][/b][/size][/color][/center]\n')">截图赏析</button></li>
         <li><button type="button" class="form__button form__button--text" x-on:click="convertImages()">转换大图</button></li>
-        <li><button type="button" class="form__button form__button--text" x-on:click="showoneclickbuttons = ! showoneclickbuttons">一键模板</button></li>
-        <menu class="bbcode-input__icon-bar" x-cloak x-show="showoneclickbuttons">
+        <li><button type="button" class="form__button form__button--text" x-on:click="showOneClickButtons = ! showOneClickButtons">一键模板</button></li>
+        <menu class="bbcode-input__icon-bar" x-cloak x-show="showOneClickButtons">
             <li><button type="button" class="form__button form__button--text" x-on:click="ConfirmMovieTrans">影视模板</button></li>
             <li><button type="button" class="form__button form__button--text" x-on:click="ConfirmMusicTrans">音乐模板</button></li>
             <li><button type="button" class="form__button form__button--text" x-on:click="ConfirmVoiceTrans">有声模板</button></li>
