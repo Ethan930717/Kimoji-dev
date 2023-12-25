e2ef919434774c53f1e2501586d56918f5df575d
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-25 16:43:37 +0800

    1

--- resources/views/page/friendsiteimage.blade.php
+++ resources/views/page/friendsiteimage.blade.php
@@ -50,7 +50,7 @@
             KIMOJI 画廊今日共计展出作品: {{ count($images) }} 幅
         </h2>
     </section>
-    <div x-data="imageGallery(@json($images))">
+    <div x-data="imageGallery({{$images}})">
         <div class="stats__panels" >
             @foreach ($images as $index => $image)
                 <div class="image-container">
