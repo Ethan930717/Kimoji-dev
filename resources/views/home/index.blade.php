ad90b318ebc3751039be94f6dfd44fa72c168af2
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-27 01:40:18 +0800

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,5 +7,7 @@
     @include('blocks.featured')
     @include('blocks.poll')
     @include('blocks.top_torrents')
+{{--    @include('blocks.latest_topics')
+    @include('blocks.latest_posts')--}}
     @include('blocks.online')
 @endsection
