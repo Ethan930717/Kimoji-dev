50b560cea9047b9e6793423719ee11c49228a8d8
HDVinnie <hdinnovations@protonmail.com>
2023-12-27 18:29:28 +0000

    PHP Style Change (Laravel Pint CI)

--- app/Http/Controllers/MusicUploadController.php
+++ app/Http/Controllers/MusicUploadController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Log;
-use Exception;
 
 class MusicUploadController extends Controller
 {
@@ -22,27 +20,26 @@ class MusicUploadController extends Controller
             'filetype' => 'required|string|in:audio/*', // 示例文件类型
         ]);
 
-        $filePath = 'uploads/' . $request->filename;
+        $filePath = 'uploads/'.$request->filename;
         $filetype = $request->filetype;
 
         // 检查文件类型
-        if (!in_array($filetype, ['audio/*'])) {
+        if (!\in_array($filetype, ['audio/*'])) {
             return response()->json(['error' => '不支持的文件类型'], 400);
         }
 
         $disk = Storage::disk('s3');
         $command = $disk->getDriver()->getAdapter()->getClient()->getCommand('PutObject', [
-            'Bucket' => env('AWS_BUCKET'),
-            'Key' => $filePath,
+            'Bucket'      => env('AWS_BUCKET'),
+            'Key'         => $filePath,
             'ContentType' => $filetype,
-            'ACL' => 'public-read',
+            'ACL'         => 'public-read',
         ]);
 
         $presignedRequest = $disk->getDriver()->getAdapter()->getClient()->createPresignedRequest($command, '+20 minutes');
 
-        $presignedUrl = (string)$presignedRequest->getUri();
+        $presignedUrl = (string) $presignedRequest->getUri();
 
         return response()->json(['url' => $presignedUrl]);
     }
-
 }
