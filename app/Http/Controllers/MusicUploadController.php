46b645286e7016b03b47bc82d157fc37686255dd
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2023-12-27 22:51:07 +0800

    Update MusicUploadController.php

--- app/Http/Controllers/MusicUploadController.php
+++ app/Http/Controllers/MusicUploadController.php
@@ -15,6 +15,7 @@ class MusicUploadController extends Controller
         // 注意这里的路径改变
         return view('torrent.music-upload'); // 更新视图路径
     }
+
     public function upload(Request $request)
     {
         try {
@@ -22,7 +23,7 @@ class MusicUploadController extends Controller
                 'musicfile' => 'required|file|max:102400' // 最大 100MB
             ]);
 
-            Log::info('开始文件上传');
+            Log::info('开始文件上传', ['user' => $request->user()]);
 
             $file = $request->file('musicfile');
             $date = Carbon::now()->format('Y-m-d');
@@ -31,19 +32,31 @@ class MusicUploadController extends Controller
             $fileName = $randomName.'.'.$extension;
             $filePath = "{$date}/{$fileName}";
 
-            Log::info("文件上传到S3: {$filePath}");
+            Log::info("文件上传到S3的路径: {$filePath}");
+
+            // 检查文件是否已经存在
+            if (Storage::disk('s3')->exists($filePath)) {
+                Log::warning("文件 {$filePath} 已存在于 S3 上。");
+            }
 
             Storage::disk('s3')->put($filePath, file_get_contents($file), 'public');
 
-            $fileUrl = Storage::disk('s3')->url($filePath);
+            if (Storage::disk('s3')->exists($filePath)) {
+                $fileUrl = Storage::disk('s3')->url($filePath);
+                Log::info("文件上传成功，可访问 URL: {$fileUrl}");
+            } else {
+                Log::error("文件 {$filePath} 在 S3 上不存在，上传可能失败。");
+            }
 
-            Log::info("文件上传成功: {$fileUrl}");
-
-            return response()->json(['url' => $fileUrl]);
+            return response()->json(['url' => $fileUrl ?? '']);
         } catch (Exception $e) {
-            Log::error("文件上传失败: ".$e->getMessage());
+            Log::error("文件上传失败: ".$e->getMessage(), [
+                'file' => $request->file('musicfile')->getClientOriginalName(),
+                'size' => $request->file('musicfile')->getSize(),
+                'mime' => $request->file('musicfile')->getMimeType(),
+            ]);
 
-            return response()->json(['error' => '上传失败'], 500);
+            return response()->json(['error' => '上传失败', 'message' => $e->getMessage()], 500);
         }
     }
 }
