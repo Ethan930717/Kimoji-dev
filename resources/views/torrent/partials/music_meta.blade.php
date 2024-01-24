bf4b56d0c336a985de503d26caf1ef4f41cf3703
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-01-25 00:23:48 +0800

    1

--- resources/views/torrent/partials/music_meta.blade.php
+++ resources/views/torrent/partials/music_meta.blade.php
@@ -18,7 +18,7 @@
             <img
                     src="{{ file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg') ? url('/files/img/torrent-cover_'.$torrent->id.'.jpg') : 'https://via.placeholder.com/500x500' }}"
                     class="meta__poster"
-                    @click="openImageModal('{{ file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg') ? url('/files/img/torrent-cover_'.$torrent->id.'.jpg') : 'https://via.placeholder.com/500x500' }}')"
+                    onclick="openImageModal('{{ file_exists(public_path().'/files/img/torrent-cover_'.$torrent->id.'.jpg') ? url('/files/img/torrent-cover_'.$torrent->id.'.jpg') : 'https://via.placeholder.com/500x500' }}')"
             >
 
         </a>
@@ -46,7 +46,8 @@
                     {{ $torrent?->distributor->name ?? '未知风格' }}
                 </a>
             </li>
-        </ul>        @php
+        </ul>
+        @php
             $description = $torrent->description;
             $pattern = '/专辑介绍\]\[size=16\]\[color=white\]\[center\](.*?)\[\/center\]/s';
             $matches = [];
@@ -70,16 +71,81 @@
 
                 // 提取文件名（不含后缀）
                 $pathInfo = pathinfo($musicUrl);
-                if (isset($pathInfo['filename'])) {
-                    $musicName = $pathInfo['filename'];
+                if (isset($pathInfo['basename'])) {
+                    // 解码 URL 编码的字符串
+                    $decodedName = urldecode($pathInfo['basename']);
+
+                    // 移除文件后缀
+                    $musicName = preg_replace('/\.[^.]+$/', '', $decodedName);
                 }
             }
         @endphp
+        @php
+            $description = $torrent->description;
+            $pattern = '/歌曲列表\]\[size=16\]\[color=white\]\[center\](.*?)\[\/center\]/s';
+            $matches = [];
+
+            if (preg_match($pattern, $description, $matches)) {
+                $songListContent = html_entity_decode($matches[1]);
+                // 分割成单独的行
+                $songs = preg_split('/\r\n|\r|\n/', $songListContent);
+                // 提取每一行中的歌曲名称
+                $songNames = array_map(function($line) {
+                    if (preg_match('/^\d+\.\s+(.*)\s+-\s+\[.+\]$/', $line, $matches)) {
+                        return $matches[1]; // 返回歌曲名称
+                    }
+                    return null;
+                }, $songs);
+                $songNames = array_filter($songNames); // 移除空值
+            } else {
+                $songNames = [];
+            }
+        @endphp
+        @php
+            $description = $torrent->description;
+            $spectrogramPattern = '/\[spoiler=截图赏析\].*?\[img\](.*?)\[\/img\]/s';
+            $spectrogramUrl = '';
+
+            if (preg_match($spectrogramPattern, $description, $matches)) {
+                $spectrogramUrl = $matches[1];
+            }
+        @endphp
+
+
+        <div class="meta__chips">
+            <section class="meta__chip-container">
+                <h2 class="meta__heading">歌曲列表</h2>
+                @foreach($songNames as $songName)
+                    <article class="meta-chip-wrapper">
+                        <div class="meta-chip">
+                            <h2 class="meta-chip__name">{{ $songName }}</h2>
+                        </div>
+                    </article>
+                @endforeach
+            </section>
+            @if ($spectrogramUrl)
+                <section class="meta__chip-container">
+                    <h2 class="meta__heading">频谱分析</h2>
+                    <article class="meta-chip-wrapper">
+                        <div class="meta-chip">
+                            <img
+                                src="{{ $spectrogramUrl }}"
+                                class="meta-chip__image"
+                                alt="频谱分析"
+                                onclick="openImageModal('{{ $spectrogramUrl }}')"
+                                style="cursor: pointer;"
+                            />
+                        </div>
+                    </article>
+                </section>
+            @endif
+        </div>
 
         <div id="aplayer-container"
              data-cover="{{ url('img/kimoji-music.webp') }}"
              data-name="{{ $musicName }}"
              data-url="{{ $musicUrl }}"
+             data-artist="{{ $singerName }}"
              data-lrc="{{ $lrcUrl }}">
             <div id="aplayer" class="aplayer"></div>
         </div>
