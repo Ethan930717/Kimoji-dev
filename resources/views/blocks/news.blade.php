359d589d91adca50f916e191182a2a2f96aa0cc6
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-01 23:57:12 +0800

    Update news.blade.php

--- resources/views/blocks/news.blade.php
+++ resources/views/blocks/news.blade.php
@@ -1,5 +1,10 @@
 @foreach ($articles as $article)
     <section class="panelV2" x-data="{ show: {{ $article->newNews }} }">
+        <h2 class="panel__count">
+            <i class="{{ config('other.font-awesome') }} fa-star"></i>
+            KIMOJI当前收录：歌手 {{ $artistsCount }} ， 专辑 {{ $albumsCount }} ， 歌曲 {{ $songsCount }}
+            <i class="{{ config('other.font-awesome') }} fa-star"></i>
+        </h2>
         <header class="panel__header" x-on:click="show = !show" style="cursor: pointer">
             <h2 class="panel__heading panel__heading--centered">
                 @if ($article->newNews)
