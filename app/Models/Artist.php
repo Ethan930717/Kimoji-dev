432bece84228a769983b7d6b0032e5d7ae82cffe
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-07 13:09:42 +0800

    Update Artist.php

--- app/Models/Artist.php
+++ app/Models/Artist.php
@@ -9,9 +9,10 @@ class Artist extends Model
 {
     use HasFactory;
 
+    public $timestamps = false; // 如果您的表没有这些字段，请添加这行
+
     // 设置模型的 fillable 或 guarded 属性，以便在使用模型的 create 或 update 方法时允许批量赋值
     protected $fillable = ['name', 'birthday', 'deathday', 'member', 'country', 'label', 'genre', 'biography', 'image_url'];
-
     // 如果您有日期字段，您可以添加它们到 $dates 属性中，以便它们将自动被转换为 Carbon 实例
     protected $dates = ['birthday', 'deathday'];
 
