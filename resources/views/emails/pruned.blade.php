9eec9bda55b79e217a02978c6f350aa938f54e5a
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-17 23:20:26 +0800

    Update pruned.blade.php

--- resources/views/emails/pruned.blade.php
+++ resources/views/emails/pruned.blade.php
@@ -1,4 +1,4 @@
 @component('mail::message')
 # {{ __('email.pruned-header') }}!
-由于未在规定时间内达到站点最低保种要求，您的账户已从 {{ config('other.title') }} 永久删除！
+由于未在规定时间内达到站点最低保种要求，我们只能遗憾的请您离开 {{ config('other.title') }} ，山高水远，有缘再见！
 @endcomponent
