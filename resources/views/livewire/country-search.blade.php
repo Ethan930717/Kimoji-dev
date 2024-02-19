37a3a9390da5f63245be314f83ed146b94ca8549
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-19 19:17:37 +0800

    Update country-search.blade.php

--- resources/views/livewire/country-search.blade.php
+++ resources/views/livewire/country-search.blade.php
@@ -1,20 +1,20 @@
 <div>
     <input type="text" wire:model="search" placeholder="{{ __('搜索国家') }}" class="form__text" />
 
-    <div class="panel__body">
-        <ul class="list">
+    <div
+        class="panel__body"
+        style="display: grid; grid-template-columns: repeat(auto-fit, minmax(140px, 1fr)); gap: 2rem;">
             @forelse ($countries as $country)
-                <li>
-                    <a href="{{ route('artists.country.show', ['country_name' => urlencode($country->country)]) }}">
-                        <div class="country-image-container">
-                            <img src="/img/country/{{ $country->country }}.webp" alt="{{ $country->country }}" />
-                        </div>
-                        <div>{{ $country->country }}</div>
-                    </a>
-                </li>
+            <figure style="display: flex; flex-direction: column; align-items: center">
+                <a href="{{ route('artists.country.show', ['country_name' => urlencode($country->country)]) }}">
+                    <div class="country-image-container">
+                        <img src="/img/country/{{ $country->country }}.webp" alt="{{ $country->country }}" />
+                    </div>
+                    <figcaption>{{ $country->country }}</figcaption>
+                </a>
+            </figure>
             @empty
                 <p>{{ __('未找到国家信息') }}</p>
             @endforelse
-        </ul>
     </div>
 </div>
