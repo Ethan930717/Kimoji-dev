4d8387412148e711eadf702bfce03d167b6c8826
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-19 16:46:28 +0800

    1

--- resources/views/blocks/country.blade.php
+++ resources/views/blocks/country.blade.php
@@ -6,7 +6,7 @@
         <div x-data>
             <ul class="featured-carousel" x-ref="featured">
                 @foreach ($countries as $country)
-                    <li class="featured-carousel__slide" style="margin-right: 8px; margin-left: 8px;">
+                    <li class="featured-carousel__slide" style="margin-right: 8px; margin-left: 8px; flex:none">
                         <a href="{{ route('artists.country.show', ['country_name' => urlencode($country->country)]) }}">
                             <div class="country-image-container">
                                 <img src="/img/country/{{ $country->country }}.webp" alt="{{ $country->country }}" />
@@ -16,28 +16,6 @@
                     </li>
                 @endforeach
             </ul>
-            <nav class="featured-carousel__nav">
-                <button
-                    class="featured-carousel__previous"
-                    x-on:click="
-                    $refs.featured.scrollLeft == 16
-                        ? ($refs.featured.scrollLeft = $refs.featured.scrollWidth)
-                        : ($refs.featured.scrollLeft -= ($refs.featured.children[0].offsetWidth + 16) / 2 + 2)
-                "
-                >
-                    <i class="{{ \config('other.font-awesome') }} fa-angle-left"></i>
-                </button>
-                <button
-                    class="featured-carousel__next"
-                    x-on:click="
-                    $refs.featured.scrollLeft == $refs.featured.scrollWidth - $refs.featured.offsetWidth - 16
-                        ? ($refs.featured.scrollLeft = 0)
-                        : ($refs.featured.scrollLeft += ($refs.featured.children[0].offsetWidth + 16) / 2 + 2)
-                "
-                >
-                    <i class="{{ \config('other.font-awesome') }} fa-angle-right"></i>
-                </button>
-            </nav>
         </div>
     </section>
 @endif
