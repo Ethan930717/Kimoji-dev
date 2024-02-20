e2ea37d826e30b194a87b8fabf67e955f20e16bd
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-02-20 22:41:24 +0800

    imageacche

--- app/Http/Controllers/ImageGalleryController.php
+++ app/Http/Controllers/ImageGalleryController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\FriendsiteImage;
+use Illuminate\Support\Facades\Cache;
+
 
 class ImageGalleryController extends Controller
 {
     public function showGallery()
     {
-        $images = FriendsiteImage::all();
+        $images = Cache::remember('friendsite_images', 60, function () {
+            return FriendsiteImage::all();
+        });
 
         return view('page.friendsiteimage', compact('images'));
     }
+
 }
