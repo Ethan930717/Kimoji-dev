b84f64cf0af79763453d662ce3e8cd7e0dfad56d
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-03-02 20:26:47 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -81,7 +81,7 @@ Route::middleware('language')->group(function (): void {
         // General
         Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home.index');
         Route::get('/gallery', [ImageGalleryController::class, 'showGallery']);
-        Route::get('/listen/{filename}', [WebDAVController::class, 'listen'])->where('filename', '.*');
+        Route::get('/listen', [WebDAVController::class, 'index'])->name('listen.index');
 
         // Articles System
         Route::prefix('articles')->group(function (): void {
