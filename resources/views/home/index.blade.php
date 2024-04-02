876862b5ff662e186a3a486d777a6f8a6ebcefff
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-04-02 15:20:24 +0800

    chatroom

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,6 +7,10 @@
     @include('blocks.country', ['countries' => $countries])
     @include('blocks.featured')
     @include('blocks.poll')
+    <div id="vue">
+        @include('blocks.chat')
+    </div>
+    @vite('resources/js/unit3d/chat.js')
     @include('blocks.top_torrents')
 {{--    @include('blocks.latest_topics')--}}
     @include('blocks.latest_posts')
