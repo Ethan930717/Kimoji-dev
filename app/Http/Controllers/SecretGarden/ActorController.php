5a4a76df9f68a90e8e6b4948a4672fbc4fd0b0b4
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-05-16 15:46:39 +0800

    Update ActorController.php

--- app/Http/Controllers/SecretGarden/ActorController.php
+++ app/Http/Controllers/SecretGarden/ActorController.php
@@ -6,24 +6,21 @@ use App\Models\Actor;
 use Illuminate\Http\Request;
 use App\Models\Torrent;
 
-
 class ActorController extends Controller
 {
     public function index()
     {
         $actors = Actor::all();
-
         return view('actors.index', compact('actors'));
     }
 
     public function show($id)
     {
-        $actor = Actors::findOrFail($id);
+        $actor = Actor::findOrFail($id); // 修改为正确的模型名称
         $torrents = Torrent::where('name', 'like', '%'.$actor->name.'%')
             ->where('category_id', 3)
             ->get();
 
         return view('actors.show', compact('actor', 'torrents'));
     }
-
 }
