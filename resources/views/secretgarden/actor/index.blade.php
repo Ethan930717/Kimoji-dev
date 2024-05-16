1f895af199ddfa65da4ddbde6c7404158937094e
hudan717 <51702667+Ethan930717@users.noreply.github.com>
2024-05-16 17:01:51 +0800

    Update index.blade.php

--- resources/views/secretgarden/actor/index.blade.php
+++ resources/views/secretgarden/actor/index.blade.php
@@ -1,19 +1,18 @@
-@extends('layouts.app')
+@extends('layout.default')
 
 @section('title')
-    <title>{{ __('actors.title') }} - {{ config('other.title') }}</title>
+    <title>{{ __('artists.title') }} - {{ config('other.title') }}</title>
 @endsection
 
 @section('meta')
-    <meta name="description" content="{{ __('actors.description') }}" />
+    <meta name="description" content="{{ __('artists.description') }}" />
 @endsection
 
 @section('breadcrumbs')
     <li class="breadcrumb--active">
-        {{ __('actors.title') }}
+        {{ __('artists.title') }}
     </li>
 @endsection
-
 @section('breadcrumbs')
     <li class="breadcrumbV2">
         <a class="breadcrumb__link" href="{{ route('home.index') }}">
@@ -21,7 +20,7 @@
         </a>
     </li>
     <li class="breadcrumb--active">
-        {{ __('actors.title') }}
+        {{ __('artists.title') }}
     </li>
 @endsection
 
