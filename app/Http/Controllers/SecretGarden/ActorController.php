c8c88929c95e6067eecb505b8fb21764a92c4b25
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2024-06-06 16:49:49 +0800

    Update ActorController.php

--- app/Http/Controllers/SecretGarden/ActorController.php
+++ app/Http/Controllers/SecretGarden/ActorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\SecretGarden;
 
 use App\Http\Controllers\Controller;
 use App\Models\Actor;
+use App\Models\Video; 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 
