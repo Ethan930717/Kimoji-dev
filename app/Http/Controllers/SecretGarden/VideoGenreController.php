9067f25d58294640d782b653bfa01789714d0459
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2024-06-09 04:02:52 +0800

    Update VideoGenreController.php

--- app/Http/Controllers/SecretGarden/VideoGenreController.php
+++ app/Http/Controllers/SecretGarden/VideoGenreController.php
@@ -3,25 +3,24 @@
 namespace App\Http\Controllers\SecretGarden;
 
 use App\Http\Controllers\Controller;
+use App\Models\Video;
 use App\Models\VideoGenre;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Cache;
 
 class VideoGenreController extends Controller
 {
     public function index(Request $request)
     {
+        \Log::info('Entered VideoGenreController@index');
         $search = $request->get('search', '');
 
-        // 生成缓存键
         $cacheKey = 'video_genres_' . md5($search);
 
-        // 从缓存中获取数据，缓存时间设置为10分钟
         $genres = Cache::remember($cacheKey, 600, function () use ($search) {
             return VideoGenre::when($search, function($query, $search) {
                 return $query->where('name', 'like', "%{$search}%");
             })
-                ->withCount('videos') // 使用关系表统计视频数量
+                ->withCount('videos')
                 ->paginate(50);
         });
 
@@ -39,14 +38,11 @@ class VideoGenreController extends Controller
                 ->from('video_genre_video')
                 ->where('genre_id', $id);
         })->orderBy($sortField, $sortDirection)
-            ->paginate(50);
+            ->paginate(20);
 
         return view('secretgarden.video_genres.show', compact('genre', 'videos', 'sortField', 'sortDirection'));
     }
 }
-
-
-
 
 
 
