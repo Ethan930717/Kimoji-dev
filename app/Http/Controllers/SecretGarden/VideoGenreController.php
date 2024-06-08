f2feaf69bbb84030d65a65782d5beb6b35ed922f
Ethan930717 <51702667+Ethan930717@users.noreply.github.com>
2024-06-09 04:21:58 +0800

    cache

--- app/Http/Controllers/SecretGarden/VideoGenreController.php
+++ app/Http/Controllers/SecretGarden/VideoGenreController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Video;
 use App\Models\VideoGenre;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
 
 class VideoGenreController extends Controller
 {
@@ -33,12 +35,16 @@ class VideoGenreController extends Controller
         $sortField = $request->get('sort', 'release_date');
         $sortDirection = $request->get('direction', 'desc');
 
-        $videos = Video::whereIn('id', function($query) use ($id) {
-            $query->select('video_id')
-                ->from('video_genre_video')
-                ->where('genre_id', $id);
-        })->orderBy($sortField, $sortDirection)
-            ->paginate(20);
+        $cacheKey = 'genre_videos_' . $id . '_' . $sortField . '_' . $sortDirection;
+
+        $videos = Cache::remember($cacheKey, 600, function () use ($id, $sortField, $sortDirection) {
+            return Video::whereIn('id', function($query) use ($id) {
+                $query->select('video_id')
+                    ->from('video_genre_video')
+                    ->where('genre_id', $id);
+            })->orderBy($sortField, $sortDirection)
+                ->paginate(20);
+        });
 
         return view('secretgarden.video_genres.show', compact('genre', 'videos', 'sortField', 'sortDirection'));
     }
